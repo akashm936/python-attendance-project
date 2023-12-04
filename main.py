c8a9450f451e14f4cb0e45c28ad66c3167b5a4a6
Akash Morwal <akashbhau101@gmail.com>
2023-12-04 15:01:52 +0530

    3 img done

--- main.py
+++ main.py
@@ -9,13 +9,30 @@ class Face_Recognization_System:
         self.root=root
         self.root.geometry("1400x740+0+0")
         self.root.title("face recognization Sysytem")
-
+# this is first img
         img=Image.open(r"D:\python face project\images\headimg.jpg")
         img=img.resize((500,130),Image.ADAPTIVE)
         self.photoimg=ImageTk.PhotoImage(img)
 
         f_lbl=Label(self.root,image=self.photoimg)
         f_lbl.place(x=0,y=0,width=500,height=130)
+# this is second img
+
+        img1=Image.open(r"D:\python face project\images\headimg.jpg")
+        img1=img1.resize((500,130),Image.ADAPTIVE)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=500,y=0,width=500,height=130)
+
+# this is third img
+        img2=Image.open(r"D:\python face project\images\headimg.jpg")
+        img2=img2.resize((500,130),Image.ADAPTIVE)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=1000,y=0,width=500,height=130)
+        
 
 if __name__ == "__main__":
     root=Tk()
